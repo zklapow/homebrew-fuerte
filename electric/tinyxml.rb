c35f7866595e90970a59d9be47735923c6ec66dd
William Woodall <wjwwood@gmail.com>
2011-10-19 17:23:54 -0500

    Updating tinyxml to be keg_only

--- electric/tinyxml.rb
+++ electric/tinyxml.rb
@@ -6,7 +6,7 @@ class Tinyxml < Formula
   md5 '35efe59f25b7980a6f3ec0118908cc11'
   version '2.6.2'
 
-  # keg_only "To keep versions independant and not polute /usr/local."
+  keg_only "To keep versions independant and not polute /usr/local."
 
   depends_on 'cmake'
 
