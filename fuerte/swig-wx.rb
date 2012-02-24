34d21cfa4362beaaf937b42b63bd333901d0f78d
William Woodall <wjwwood@gmail.com>
2012-02-24 10:31:24 -0600

    Fixes for swig-wx

--- fuerte/swig-wx.rb
+++ fuerte/swig-wx.rb
@@ -1,14 +1,17 @@
 require 'formula'
 
-class Swig-wx < Formula
+class SwigWx < Formula
   url 'https://github.com/wg-debs/swig-wx/tarball/debian/ros_fuerte_1.3.29_oneiric'
   homepage 'http://ros.org/wiki/wxswig'
   md5 'edd2f143ff51295f9ffa7e1f9ecab358'
   version '1.3.29'
 
+  keg_only "Only used in building catkin stuff"
+
   def install
     ENV.universal_binary
     system "./configure", "--prefix=#{prefix}"
+    system "make"
     system "make install"
   end
 end
