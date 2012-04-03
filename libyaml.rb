498e7776758e064ba717ebf4f77b7120d96ee986
William Woodall <wjwwood@gmail.com>
2012-04-02 19:25:10 -0500

    Update libyaml.rb

--- libyaml.rb
+++ libyaml.rb
@@ -5,20 +5,12 @@ class Libyaml < Formula
   homepage 'http://pyyaml.org/wiki/LibYAML'
   md5 '36c852831d02cf90508c29852361d01b'
 
-  def options
-    [
-      ["--no-universal", "Build for 32 bit Intel only."],
-    ]
-  end
-
   def install
     args = ["--prefix=#{prefix}"]
 
-    if not ARGV.build_no_universal?
-      ENV['CFLAGS'] = "-arch i386 -arch x86_64"
-      ENV['LDFLAGS'] = "-arch i386 -arch x86_64"
-      args << "--disable-dependency-tracking"
-    end
+    ENV['CFLAGS'] = "-arch i386 -arch x86_64"
+    ENV['LDFLAGS'] = "-arch i386 -arch x86_64"
+    args << "--disable-dependency-tracking"
 
     system './configure', *args
     system "make install"
