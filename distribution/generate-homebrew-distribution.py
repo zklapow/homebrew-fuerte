768386bdb6a0d2bc819307a523e8204b8aad24e0
William Woodall <wjwwood@gmail.com>
2012-04-19 16:35:36 -0500

    Adding the distribution generation files.

---
+++ distribution/generate-homebrew-distribution.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import os
+import optparse
+import sys
+
+from tempfile import mkdtemp
+from subprocess import Popen, CalledProcessError
+
+try:
+    import yaml
+except ImportError:
+    print("Please install python-yaml", file=sys.stderr)
+    sys.exit(1)
+
+try:
+    import vcstools
+except ImportError:
+    print("Please sudo pip install -U vcstools", file=sys.stderr)
+    sys.exit(2)
+
+def parse_options():
+    import argparse
+    parser = argparse.ArgumentParser(description='Creates/updates binaries for each repo described in a gbp-repo file.')
+    parser.add_argument('--working', help='A scratch build path. Default: %(default)s',
+                        default=mkdtemp())
+    parser.add_argument('--gbp-repo-file', help='File path or URL to the gbp-repo file that gives the information about each repo.',
+                        default='')
+    parser.add_argument('--build_binary', help='Weather or not to generate binaries for distribution or not.', action='store_true')
+    parser.add_argument('--redo', help='If this is set repos that have previously been generated will be updated and regenerated.',
+                        action='store_true')
+    parser.add_argument('--resume-on-error', help='Will continue through repos even if one fails.', action='store_true')
+    return parser.parse_args()
+
+def fetch_gbp_file(url, directory):
+    from urllib import urlopen
+    gbp_file_path = os.path.join(directory, 'distribution.yaml')
+    gbp_file = open(gbp_file_path, 'w')
+    gbp_file_data = urlopen(url).read()
+    gbp_file.write(gbp_file_data)
+    return gbp_file_path
+
+def parse_gbp_yaml(file_path):
+    from yaml import load
+    gbp_yaml = load(open(file_path, 'r'))
+    return gbp_yaml
+
+def make_directory(directory):
+    """Creates the directory if it doesn't exist yet"""
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def detect_scm_type(url):
+    # TODO: determine this by the url, or hope they had a scm tag to the release file
+    return 'git'
+
+def call(working_dir, command, pipe=None):
+    print('+ cd %s && ' % working_dir + ' '.join(command))
+    process = Popen(command, stdout=pipe, stderr=pipe, cwd=working_dir)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        raise CalledProcessError(retcode, command)
+    if pipe:
+        return output
+
+def process_repo(repo, ros_distro, repos_dir, build_binary=False, redo=False):
+    # Create the repo directory if needed
+    repo_dir = os.path.join(repos_dir, repo['name'])
+    repo['working_dir'] = repo_dir
+    print("+++ Processing repository: [%(name)s] from `%(url)s` in directory [%(working_dir)s]"%repo)
+    if not redo:
+        if os.path.exists(os.path.join(repo_dir, 'GENERATED')):
+            print("  [%(name)s] Distribution generation has already been done on this repo, and redo was not set, skipping..."%repo)
+            return
+    make_directory(repo_dir)
+    # Setup the sources
+    src_dir = os.path.join(repo_dir, 'sources')
+    repo['src_dir'] = src_dir
+    # Detect the scm type
+    scm_type = detect_scm_type(repo['url'])
+    vcs_client = vcstools.VcsClient(scm_type, src_dir)
+    # If the source directory exists, try to update it
+    source_ok = False
+    if os.path.exists(src_dir):
+        # If the source directory contains a repo of the correct type
+        # and if the url's match, try to update
+        if vcs_client.detect_presence() and vcs_client.get_url() == repo['url']:
+            print("  [%(name)s] Sources exist, attempting to update..."%repo)
+            vcs_client.update(vcs_client.get_version())
+            source_ok = True
+        # Else remove the directory so it can get a fresh checkout
+        else:
+            # Why did it fail?
+            if not vcs_client.detect_presence():
+                print("  [%(name)s] Sources exist, but scm type doesn't match, deleting..."%repo)
+            elif vcs_client.get_url() != repo['url']:
+                print("  [%(name)s] Sources exist, but url's don't match, deleting..."%repo)
+            else:
+                print("  [%(name)s] Sources exist, but could not update, deleting..."%repo)
+            # Remove the tree
+            from shutil import rmtree
+            rmtree(src_dir)
+            # Make a new directory to checkout to
+            make_directory(src_dir)
+    # If the update failed, check it out
+    if not source_ok:
+        print("  [%(name)s] Fetching sources to %(src_dir)s ..."%repo)
+        vcs_client.checkout(repo['url'])
+    # Generate a Homebrew formula using the 'generate-homebrew-formula' command
+    script_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'generate-homebrew-formula.py')
+    stack_path = os.path.abspath(os.path.join(src_dir, 'stack.yaml'))
+    call(repo_dir, ['python', script_path, ros_distro, stack_path, repo['url']])
+    # If the call was successful, put a GENEREATED file in the directory
+    open(os.path.join(repo_dir, 'GENERATED'), 'w').close()
+
+def generate_variant(variant, ros_distro):
+    pass
+
+def process_repos(gbp_yaml, args):
+    # Get the ROS distribution we are building for:
+    ros_distro = gbp_yaml['release-name']
+    # For each repo we will:
+    #  * Make a subdirectory in the working directory for it
+    #  * Checkout the repo into the source directory of the repo directory
+    #  * Generate a Homebrew Formula from the stack.yaml using rosdep
+    #  * Generate a pip requirements file from the stack.yaml using rosdep
+    #  * (binary)Install pip dependencies
+    #  * (binary)Install homebrew formula
+    #  * (binary)Create a homebrew bottle
+    repos = gbp_yaml['gbp-repos']
+    make_directory(os.path.join(args.working, 'repos'))
+    successes = []
+    errors = {}
+    for repo in repos:
+        try:
+            process_repo(repo, ros_distro, os.path.join(args.working, 'repos'), args.build_binary, args.redo)
+        except Exception as e:
+            print("  [%s] Error occurred while processing: "%repo['name'])
+            import traceback
+            traceback.print_exc()
+            errors[repo['name']] = e
+            if not args.resume_on_error:
+                sys.exit(3)
+            else:
+                print("Resuming...")
+                continue
+        successes.append(repo['name'])
+    # Report on the repo generation
+    print("+++")
+    print("+++ %i out of %i repos succeeded in generating distribution files."%(len(successes), len(repos)))
+    print("+++ Repos that failed: ")
+    for repo_name in errors.keys():
+        print("  %s"%repo_name)
+    print("+++")
+    return
+    # For each variant we will:
+    #  * Generate a Homebrew formula
+    variants = gbp_yaml['variants']
+    for variant in variants:
+        generate_variant(variant, ros_distro)
+
+def main(args):
+    # Make the working directory if it doesn't already exist
+    print("Using working directory: [%s]"%args.working)
+    make_directory(args.working)
+    gbp_yaml = None
+    # If a gbp_repo_file was specified fetch it and load the yaml
+    if args.gbp_repo_file:
+        print("Fetching `%s` to [%s]"%(args.gbp_repo_file, os.path.join(args.working, 'distribution.yaml')))
+        gbp_yaml = parse_gbp_yaml(fetch_gbp_file(args.gbp_repo_file, args.working))
+    # Otherwise you need to try and use the distribution.yaml in the working dir
+    elif os.path.exists(os.path.join(args.working, 'distribution.yaml')):
+        # It exists, use it
+        print("No gbp_repo_file specified, using [%s]"%os.path.join(args.working, 'distribution.yaml'))
+        gbp_yaml = parse_gbp_yaml(os.path.join(args.working, 'distribution.yaml'))
+    else:
+        # No gbp_repo_file specified and none in the working dir, error
+        print("No gbp_repo_file provided and no distribution.yaml in the working directory, cannot continue.")
+        sys.exit(4)
+    # Use the yaml to proceed with downloading the repos, generating homebrew formula and pip requirement files
+    process_repos(gbp_yaml, args)
+
+if __name__ == "__main__":
+    main(parse_options())
---
+++ distribution/generate-homebrew-formula.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import os
+import sys
+
+from tempfile import mkdtemp
+
+try:
+    import yaml
+except ImportError:
+    print("Please install python-yaml", file=sys.stderr)
+    sys.exit(1)
+
+try:
+    import rosdep2.catkin_support
+except ImportError:
+    print("Please sudo pip install -U rosdep", file=sys.stderr)
+    sys.exit(2)
+
+def parse_stack_yaml(yaml_path):
+    stack_yaml = yaml.load(open(yaml_path))
+    deps = stack_yaml['Depends']
+    if type(deps) == str:
+        stack_yaml['Depends'] = set([x.strip() for x in deps.split(',') if len(x.strip()) > 0])
+    else:
+        stack_yaml['Depends'] = set(deps)
+    return stack_yaml
+
+def make_working(working_dir):
+    """Creates the working directory if it doesn't exist yet"""
+    if not os.path.exists(working_dir):
+        os.makedirs(working_dir)
+
+def camelcase(input):
+    temp = str(input)
+    temp.replace('-', '_')
+    return ''.join([str.capitalize(x) for x in temp.split('_')])
+
+def detect_scm_type(url):
+    # TODO: determine this by the url, or hope they had a scm tag to the release file
+    return 'git'
+
+def generate_deps(ros_distro, rosdep_keys):
+    deps = []
+    view = rosdep2.catkin_support.get_catkin_view(ros_distro, 'osx', 'lion')
+    for rosdep_key in rosdep_keys:
+        installer_key, resolution = rosdep2.catkin_support.resolve_for_osx(rosdep_key, view)
+        deps.append((installer_key, resolution))
+    return deps
+
+def generate_brew(stack_yaml, repo_url, ros_distro):
+    # Homebrew Formula Template
+    template = \
+'''
+require 'formula'
+
+class %(formula_class_name)s < Formula
+  url '%(url)s', {:using => :%(scm_type)s, :tag => '%(scm_tag)s'}
+  homepage '%(homepage)s'
+  version '%(version)s'
+
+%(depends_def)s
+
+  def install
+    ENV.universal_binary
+    system "mkdir build"
+    system "cd build && cmake .. #{std_cmake_parameters}"
+    system "cd build && make install"
+  end
+end
+'''
+    # Get the output file name
+    file_name = stack_yaml['Catkin-ProjectName']
+    # Build the template variables
+    template_vars = {}
+    template_vars['formula_class_name'] = camelcase(file_name)
+    template_vars['url'] = repo_url
+    template_vars['scm_type'] = detect_scm_type(repo_url)
+    # TODO: Maybe the scm_tag should be more portable...
+    template_vars['scm_tag'] = 'upstream/'+stack_yaml['Version']
+    template_vars['homepage'] = stack_yaml['Homepage']
+    template_vars['version'] = stack_yaml['Version']
+    # Generate a list of (installer_key, install_key) pairs for the deps using rosdep
+    deps = generate_deps(ros_distro, stack_yaml['Depends'])
+    depends_def = ""
+    pip_requirements = ""
+    for dep in deps:
+        # Get a list of the resolutions
+        if 'packages' in dep[1]:
+            resolutions = dep[1]['packages']
+        else:
+            continue
+        installer = dep[0]
+        # There could be more than one resolved
+        for resolved in resolutions:
+            # If it is pip
+            if installer == 'pip':
+                depends_def += "  depends_on '%s' => :python\n"%resolved
+                pip_requirements += resolved+'\n'
+                continue
+            # If it is homebrew
+            if installer == 'homebrew':
+                # If it contains a / it is an :alt
+                if resolved.find('/') != -1:
+                    depends_def += "  depends_on '%s' => :alt\n"%resolved
+                    continue
+                depends_def += "  depends_on '%s'\n"%resolved
+    template_vars['depends_def'] = depends_def
+    # Fill out the template
+    formula = template%template_vars
+    # Write the formula to file
+    formula_file = open(file_name+'.rb', 'w')
+    formula_file.write(formula)
+    formula_file.close()
+    # Write the pip requirements file
+    if pip_requirements:
+        pip_requirements_file = open(file_name+'_pip_requirements.txt', 'w')
+        pip_requirements_file.write(pip_requirements)
+        pip_requirements_file.close()
+
+def main(ros_distro, yaml_path, repo_url):
+    stack_yaml = parse_stack_yaml(yaml_path)
+
+    try:
+        generate_brew(stack_yaml, repo_url, ros_distro)
+    except rosdep2.catkin_support.ValidationFailed as e:
+        print(e.args[0], file=sys.stderr)
+        sys.exit(1)
+    except (KeyError, rosdep2.ResolutionError) as e:
+        if isinstance(e, KeyError):
+            rosdep_key = str(e)
+        else:
+            rosdep_key = e.rosdep_key
+        print("""Cannot resolve dependency [%(rosdep_key)s].
+
+If [%(rosdep_key)s] is catkin project, make sure it has been added to the gbpdistro file.
+
+If [%(rosdep_key)s] is a system dependency, make sure there is a rosdep.yaml entry
+for it in your sources.
+"""%locals(), file=sys.stderr)
+        sys.exit(3)
+    sys.exit(0)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print("Usage: generate-homebrew-formula <ros_distro> <path/to/stack.yaml> <repo_url>")
+        sys.exit(4)
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
