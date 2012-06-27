3c15e5a57a76c469ff1c0ae7c4f9b4d8765c9834
William Woodall <wjwwood@gmail.com>
2012-06-27 08:26:10 -0500

    Had to add a patch for octomap

--- octomap.rb
+++ octomap.rb
@@ -8,7 +8,9 @@ class Octomap < Formula
 
   depends_on 'cmake'
 
-
+  def patches
+    DATA
+  end
 
   def install
     ENV.universal_binary
@@ -18,3 +20,34 @@ class Octomap < Formula
   end
 
 end
+
+__END__
+diff --git a/include/octomap/OccupancyOcTreeBase.hxx b/include/octomap/OccupancyOcTreeBase.hxx
+index 0b7197f..360f9c0 100644
+--- a/include/octomap/OccupancyOcTreeBase.hxx
++++ b/include/octomap/OccupancyOcTreeBase.hxx
+@@ -363,12 +363,12 @@ namespace octomap {
+       if (isNodeOccupied(startingNode)){
+         // Occupied node found at origin 
+         // (need to convert from key, since origin does not need to be a voxel center)
+-        genCoords(current_key, this->tree_depth, end);
++        this->genCoords(current_key, this->tree_depth, end);
+         return true;
+       }
+     } else if(!ignoreUnknown){
+       OCTOMAP_ERROR_STR("Origin node at " << origin << " for raycasting not found, does the node exist?");
+-      genCoords(current_key, this->tree_depth, end);
++      this->genCoords(current_key, this->tree_depth, end);
+       return false;
+     }
+ 
+@@ -435,7 +435,7 @@ namespace octomap {
+       {
+          OCTOMAP_WARNING("Coordinate hit bounds in dim %d, aborting raycast\n", dim);
+          // return border point nevertheless:
+-         genCoords(current_key, this->tree_depth, end);
++         this->genCoords(current_key, this->tree_depth, end);
+          return false;
+       }
+ 
+
